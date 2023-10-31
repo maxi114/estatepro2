d82bc38d8202e833faae85594db806f10ec6d37c
unknown <mokodi114@gmail.com>
2023-10-31 14:53:50 -0400

    working on the filter

---
+++ public/js/property.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.custom-dropdown select').on('change', function () {
+        var selectedOption = $(this).find(':selected');
+        var iconOption = selectedOption.hasClass('icon-option');
+
+        if (iconOption) {
+            $('.custom-dropdown .arrow i').text(selectedOption.text());
+        } else {
+            $('.custom-dropdown .arrow i').text('arrow_drop_down');
+        }
+    });
+
+    $(".abtn").click(()=>{
+        console.log("yeah")
+    })
+});
+
+console.log("ndjwkndkwnk")
